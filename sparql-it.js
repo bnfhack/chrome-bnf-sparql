ee524f67fa305f7a008a65dc2f97001ca95eaf12
Adrien Di Mascio <Adrien.DiMascio@logilab.fr>
2016-11-30 21:32:57 +0100

    small refactoring to prepare the upcoming hackWorkPage()

--- sparql-it.js
+++ sparql-it.js
@@ -1,6 +1,19 @@
 
-function sparqlLink(role, authorUri) {
-    const authorDocsQuery = `
+function sparqlLink(query) {
+    const a = document.createElement('a');
+    a.className = 'sparql-link';
+    a.href = `http://data.bnf.fr/sparql?query=${encodeURIComponent(query)}&format=${encodeURIComponent('text/html')}`;
+    const img = document.createElement('img');
+    img.src = 'http://data.bnf.fr/data/rdf.png';
+    a.appendChild(img);
+    a.appendChild(document.createTextNode('SPARQL-it!'));
+    a.target = '_blank';
+    return a;
+}
+
+
+function authorDocsQuery(role, authorUri) {
+    return `
 PREFIX bnfroles: <http://data.bnf.fr/vocabulary/roles/>
 PREFIX rdarelationships: <http://rdvocab.info/RDARelationshipsWEMI/>
 PREFIX dcterms: <http://purl.org/dc/terms/>
@@ -17,32 +30,27 @@ SELECT DISTINCT ?manif ?title ?date WHERE {
     ?manif dcterms:date ?date.
   }
 
-}
-`;
+}`;
 
-    const a = document.createElement('a');
-    a.className = 'sparql-link';
-    a.href = `http://data.bnf.fr/sparql?query=${encodeURIComponent(authorDocsQuery)}&format=${encodeURIComponent('text/html')}`;
-    const img = document.createElement('img');
-    img.src = 'http://data.bnf.fr/data/rdf.png';
-    a.appendChild(img);
-    a.appendChild(document.createTextNode('SPARQL-it!'));
-    a.target = '_blank';
-    return a;
 }
-
 
 
 function hackDocumentSections(authorUri) {
     Array.from(document.querySelectorAll('.dtmanifs > h3 > a:first-child')).forEach(link => {
         const role = link.href.split('/').pop();
         const h3 = link.parentElement;
-        h3.insertBefore(sparqlLink(role, authorUri), link);
+        h3.insertBefore(sparqlLink(authorDocsQuery(role, authorUri)), link);
     });
-
 }
+
+
+function hackAuthorPage(pageUri) {
+    hackDocumentSections(pageUri);
+}
+
+
 
 if (document.querySelector('meta[property="og:type"]').content === 'author') {
     const pageUri = document.querySelector('link[rel=bookmark]').href;
-    hackDocumentSections(pageUri);
+    hackAuthorPage(pageUri);
 }
